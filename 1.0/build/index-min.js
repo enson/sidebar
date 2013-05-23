5ca87b41d9f07f3b7e66b5368a37252cc4202256
Enson Pu <enson@Ensons-MacBook-Pro.local>
2013-05-23 16:46:32 +0800

    update

---
+++ 1.0/build/index-min.js
@@ -0,0 +1,3 @@
+/*! sidebar - v1.0 - 2013-05-23 4:46:10 PM
+* Copyright (c) 2013 xijian.py; Licensed  */
+KISSY.add("gallery/sidebar/1.0/index",function(a,b,c){function d(a){var b=this;d.superclass.constructor.call(b,a)}return b.all,a.extend(d,c,{},{ATTRS:{}}),d},{requires:["node","base"]});
---
+++ 1.0/build/index.js
@@ -0,0 +1,36 @@
+/*
+combined files : 
+
+gallery/sidebar/1.0/index
+
+*/
+/**
+ * @fileoverview 请修改组件描述
+ * @author xijian.py<ensonfield@gmail.com>
+ * @module sidebar
+ **/
+KISSY.add('gallery/sidebar/1.0/index',function (S, Node,Base) {
+    var EMPTY = '';
+    var $ = Node.all;
+    /**
+     * 请修改组件描述
+     * @class Sidebar
+     * @constructor
+     * @extends Base
+     */
+    function Sidebar(comConfig) {
+        var self = this;
+        //调用父类构造函数
+        Sidebar.superclass.constructor.call(self, comConfig);
+    }
+    S.extend(Sidebar, Base, /** @lends Sidebar.prototype*/{
+
+    }, {ATTRS : /** @lends Sidebar*/{
+
+    }});
+    return Sidebar;
+}, {requires:['node', 'base']});
+
+
+
+
